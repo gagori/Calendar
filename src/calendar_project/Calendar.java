513b857d543a5ec68a0c505a6bbe07711c921503
user <user@DESKTOP-7HAK2IN>
2021-07-05 21:44:14 +0900

    샘플켈린더 숨기기

--- src/calendar_project/Calendar.java
+++ src/calendar_project/Calendar.java
@@ -6,11 +6,14 @@ public class Calendar {
 
 	private static final int[] MAX_DAYS =  {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};  
 	
-	public int getMaxDaysOfMonth(int month) {           //함수를 미리 만들어주기 : (int)를 입력받아서 int로 출력해준다!!
+	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month -1];
-	}
 	
-	public static void main(String[] args) {
+		}
+		
+	
+	
+	public void printSampleCalendar() {                       //버리긴 아까워서 저장해두고 출력은 안할 수 있게 함
 		System.out.println("   Hello, Calendar\n");
 		System.out.println("일 월  화  수  목  금  토");
 		System.out.println("---------------------");
@@ -18,17 +21,20 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
-		
+	}
+	
+	public static void main(String[] args) {
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		// 입력
 		Scanner scn = new Scanner(System.in);
 		Calendar calendar = new Calendar();
-		System.out.println("\n달을 입력하세요");
+		System.out.println("달을 입력하세요");
 		int month = scn.nextInt();                              
 		
 		System.out.printf("%d 월은 %d일 까지 있습니다.\n", month, calendar.getMaxDaysOfMonth(month));   //미리 만들어 두었던 함수 쓰기
 		scn.close();
+		
+		calendar.printSampleCalendar();
 		
 		}
 		
