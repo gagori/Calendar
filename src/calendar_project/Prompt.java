7f778d2ffe49475c8ec3acd591c01ea8e887f866
user <user@DESKTOP-7HAK2IN>
2021-07-06 09:38:26 +0900

    윤년

--- src/calendar_project/Prompt.java
+++ src/calendar_project/Prompt.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 
 public class Prompt {
 	
-	private final static String PROMPT="cal> ";
 	
 	public void runPrompt() {
-		Scanner scn = new Scanner(System.in);
+		Scanner scn = new Scanner(System.in);   //키보드 출력받기
 		Calendar cal = new Calendar();
 		
-		int month = 1;
+		int month = 1;         //사실 디폴트 값은 아무거나 해도 상관 없음
+		int year = 2015;
+		
+		
 		while(true) {                                
+			System.out.println("년도을 입력하세요");
+			System.out.print("YEAR> ");
+			year = scn.nextInt();
 			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.print("MONTH> ");
 			month = scn.nextInt();                       
 			if (month == -1) {                              
 				break;
@@ -21,7 +26,7 @@ public class Prompt {
 				continue;
 			}
 			
-			cal.printCalendar(2021, month);
+			cal.printCalendar(year, month);
 
 		}
 		System.out.println("Bye~");
@@ -31,6 +36,7 @@ public class Prompt {
 	public static void main(String[] args) {   //셀 실행
 		Prompt p = new Prompt();
 		p.runPrompt();
+		
 	}	
 }
 
